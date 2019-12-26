a6126f14bbc4a167a23c17f4902282b4ac0776c7
Anton Montrezor <montrezoranton991@gmail.com>
2019-12-25 20:57:56 -0500

    дороботал проект

---
+++ external.js
@@ -0,0 +1,6 @@
+function check() {
+    document.getElementById("btn-1").checked = true;
+    document.getElementById("btn-2").checked = true;
+    document.getElementById("btn-3").checked = true;
+}
+
